c330a49e2ed2346fde5f21e639160263339837ad
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-01-31 22:23:02 +0530

    Contact page

--- lib/nav-tabs/contact.dart
+++ lib/nav-tabs/contact.dart
@@ -9,142 +9,142 @@ class ContactUs extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Contact Us",
-          style: TextStyle(
-            fontFamily: 'Raleway',
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.black54,
+        // title: Text(
+        //   "Contact Us",
+        //   style: TextStyle(
+        //     fontFamily: 'Raleway',
+        //   ),
+        // ),
+        // centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: SafeArea(
-        child: ListView(
-          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-          padding: const EdgeInsets.all(20),
-          children: [
-            SizedBox(height: 20),
-            ListTile(
-              title: Text(
-                'Connect With Us',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      extendBodyBehindAppBar: true,
+      body: ListView(
+        physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        padding: const EdgeInsets.all(20),
+        children: [
+          SizedBox(height: 68),
+          ListTile(
+            title: Text(
+              'Connect With Us',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              subtitle: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () async =>
+                      await launchURL('https://twitter.com/PunePict'),
+                  icon: FaIcon(
+                    FontAwesomeIcons.twitter,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async => await launchURL(
+                      'https://www.facebook.com/PICTOfficial'),
+                  icon: FaIcon(
+                    FontAwesomeIcons.facebook,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async =>
+                      await launchURL('https://www.instagram.com/pict.pune/'),
+                  icon: FaIcon(
+                    FontAwesomeIcons.instagram,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async => await launchURL(
+                      'https://www.linkedin.com/school/pune-institute-of-computer-technology/'),
+                  icon: FaIcon(FontAwesomeIcons.linkedin),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Divider(indent: 10, endIndent: 10, thickness: 1.5),
+          SizedBox(height: 15),
+          ListTile(
+            title: Text(
+              'Email',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: InkWell(
+              onTap: () async => await launchEmail('conference@pict.edu'),
+              child: Row(
                 children: [
                   IconButton(
-                    onPressed: () async =>
-                        await launchURL('https://twitter.com/PunePict'),
-                    icon: FaIcon(
-                      FontAwesomeIcons.twitter,
+                      onPressed: () async =>
+                          await launchEmail('conference@pict.edu'),
+                      icon: Icon(
+                        Icons.mail,
+                        color: Colors.blue,
+                      )),
+                  Text(
+                    'conference@pict.edu',
+                    style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 18,
                     ),
-                  ),
-                  IconButton(
-                    onPressed: () async => await launchURL(
-                        'https://www.facebook.com/PICTOfficial'),
-                    icon: FaIcon(
-                      FontAwesomeIcons.facebook,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () async =>
-                        await launchURL('https://www.instagram.com/pict.pune/'),
-                    icon: FaIcon(
-                      FontAwesomeIcons.instagram,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () async => await launchURL(
-                        'https://www.linkedin.com/school/pune-institute-of-computer-technology/'),
-                    icon: FaIcon(FontAwesomeIcons.linkedin),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 15),
-            Divider(indent: 10, endIndent: 10, thickness: 1.5),
-            SizedBox(height: 15),
-            ListTile(
-              title: Text(
-                'Email',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: InkWell(
-                onTap: () async => await launchEmail('conference@pict.edu'),
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () async =>
-                            await launchEmail('conference@pict.edu'),
-                        icon: Icon(
-                          Icons.mail,
-                          color: Colors.blue,
-                        )),
-                    Text(
-                      'conference@pict.edu',
-                      style: TextStyle(
-                        fontFamily: 'Raleway',
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
+          ),
+          SizedBox(height: 15),
+          Divider(indent: 10, endIndent: 10, thickness: 1.5),
+          SizedBox(height: 15),
+          ListTile(
+            title: Text(
+              'Phone Numbers',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 15),
-            Divider(indent: 10, endIndent: 10, thickness: 1.5),
-            SizedBox(height: 15),
-            ListTile(
-              title: Text(
-                'Phone Numbers',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Column(
-                children: [
-                  phoneNum('02024378063'),
-                  phoneNum('02024372479'),
-                  phoneNum('02024371101'),
-                  phoneNum('02024376190'),
-                ],
+            subtitle: Column(
+              children: [
+                phoneNum('02024378063'),
+                phoneNum('02024372479'),
+                phoneNum('02024371101'),
+                phoneNum('02024376190'),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Divider(indent: 10, endIndent: 10, thickness: 1.5),
+          SizedBox(height: 15),
+          ListTile(
+            title: Text(
+              'Address',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 15),
-            Divider(indent: 10, endIndent: 10, thickness: 1.5),
-            SizedBox(height: 15),
-            ListTile(
-              title: Text(
-                'Address',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                "Pune Institute of Computer Technology.\nSurvey No. 27, Near, Trimurti Chowk, Dhankawadi.\nPune, Maharashtra, India.\nPin Code-411043",
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 18,
-                ),
+            subtitle: Text(
+              "Pune Institute of Computer Technology.\nSurvey No. 27, Near, Trimurti Chowk, Dhankawadi.\nPune, Maharashtra, India.\nPin Code-411043",
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 18,
               ),
             ),
-            SizedBox(height: 15),
-            Divider(indent: 10, endIndent: 10, thickness: 1.5),
-            SizedBox(height: 15),
-          ],
-        ),
+          ),
+          SizedBox(height: 15),
+          Divider(indent: 10, endIndent: 10, thickness: 1.5),
+          SizedBox(height: 15),
+        ],
       ),
     );
   }
